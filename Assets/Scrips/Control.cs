02a8ec3a6d08f461a2cbf65b1c637d91120fa1a3
AnnaMigitenko <migitenko@gmail.com>
2023-03-17 20:53:01 +0300

    управление капсулой и настройка коллайдеров на стар, финиш

--- Assets/Scrips/Control.cs
+++ Assets/Scrips/Control.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ControlPlayer : MonoBehaviour
+public class Control : MonoBehaviour
 {
     public Rigidbody _rig;
 
