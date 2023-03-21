408016e76aee5b20e0ef1c5af7d3d68d6ca87749
AnnaMigitenko <migitenko@gmail.com>
2023-03-21 14:02:30 +0300

    добавила код

--- Assets/Project/Scripts/PlayerController.cs
+++ Assets/Project/Scripts/PlayerController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerContoller : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     public Rigidbody _rig;
 
