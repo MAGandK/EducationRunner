cb50bcfbee9cde9c67ccb8058c755f1379250614
KIR <kirillpazitiv2020@gmail.com>
2023-04-12 20:33:57 +0300

    добил постоянное движение вперед и поправил старт

--- Assets/Project/Scripts/PlayerController.cs
+++ Assets/Project/Scripts/PlayerController.cs
@@ -11,6 +11,9 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     private float _speed = 1f;
 
+    [SerializeField]
+    private float _forwardSpeed = 0f;
+
     private Vector3 _direction;
 
     private float _moveX; // ограничение по ширине от -0.4 до 0.4
@@ -35,7 +38,9 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)// Если было сделано касание на экране, запускаем игру
+       // if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)// Если было сделано касание на экране, запускаем игру
+       
+        if (Input.GetMouseButtonDown(0))// Если было сделано касание на экране, запускаем игру
         {
             StartGame();
         }
@@ -45,16 +50,18 @@ public class PlayerController : MonoBehaviour
 
     public void MovePlayer()
     {
-        if (_isFinish)
+        //&& ||
+        if (_isFinish || !_isStarted)
         {
             return;
         }
 
         _direction = new Vector3(_fixedJoystick.Horizontal, 0, _fixedJoystick.Vertical);
-
+        
         _moveX = Mathf.Clamp(transform.position.x + _direction.x * _speed * Time.deltaTime, -0.4f, 0.4f);
 
-        _moveZ = (transform.position.z + _direction.z * _speed * Time.deltaTime);
+        //_moveZ = (transform.position.z + _direction.z * _speed * Time.deltaTime);
+        _moveZ = (transform.position.z + _forwardSpeed * Time.deltaTime);
 
         Vector3 newPosition = new Vector3(_moveX, transform.position.y, _moveZ);
 
